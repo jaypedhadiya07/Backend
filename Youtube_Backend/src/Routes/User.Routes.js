416f2f47a5589490500b0b9b24480569f06bb93f
jaypedhadiya07 <jaypedhadiya76gmail.com>
2025-09-02 16:30:53 +0530

    Refactor user routes to add JWT verification for change password, current user, account update, avatar, cover image, user channel profile, and watch history endpoints

--- Youtube_Backend/src/Routes/User.Routes.js
+++ Youtube_Backend/src/Routes/User.Routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import {
   changePassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvatarImage,
+  updateCoverImage,
 } from "../Controllers/User.Controllers.js";
 import upload from "../Middlewares/Multer.Middlewares.js";
 import { verifyJWT } from "../Controllers/Auth.Controllers.js";
@@ -24,6 +30,17 @@ router.route("/login").post(loginUser);
 //secure route
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(changePassword);
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
+router
+  .route("/cover")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
