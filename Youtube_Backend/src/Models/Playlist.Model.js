2e6660657b6d945476310a9a607866ad048eea3f
jaypedhadiya07 <jaypedhadiya76gmail.com>
2025-09-06 18:08:36 +0530

    Add models for Comment, Like, Playlist, and Tweet with schema definitions

---
+++ Youtube_Backend/src/Models/Playlist.Model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+const playListSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const PlayList = mongoose.model("PlayList", playListSchema);
